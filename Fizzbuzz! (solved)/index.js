a66912c45211a55b043b9f09baca2a48baa43079
ravi-aratchige <rmaratchige@gmail.com>
2023-02-12 13:17:29 +0530

    Removed unnecessary comments

--- Fizzbuzz! (solved)/index.js
+++ Fizzbuzz! (solved)/index.js
@@ -1,9 +1,3 @@
-// Return an array of "n" numbers
-// If number is divisible by 3, replace it with "Fizz"
-// If number is divisible by 5, replace it with "Buzz"
-// If number is divisible by both, replace it with "FizzBuzz"
-// Otherwise, return a string of the number
-
 function fizzbuzz(n) {
     let fizzBuzzArray = [];
     for (let i = 0; i < n; i++) {
