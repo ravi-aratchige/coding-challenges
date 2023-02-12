c3efda7f52968e09c6700e73466e779c55c20ccf
ravi-aratchige <rmaratchige@gmail.com>
2023-02-12 11:58:11 +0530

    "Fizzbuzz!" problem added

---
+++ Fizzbuzz!/index.js
@@ -0,0 +1,5 @@
+// Return an array of numbers
+// If number is divisible by 3, replace it with "Fizz"
+// If number is divisible by 5, replace it with "Buzz"
+// If number is divisible by both, replace it with "FizzBuzz"
+// Otherwise, return a string of the number
