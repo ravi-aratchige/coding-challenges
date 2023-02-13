835e7acb5719be1e20292120408d0626ba195354
ravi-aratchige <rmaratchige@gmail.com>
2023-02-13 07:17:24 +0530

    Added "Longest Common Prefix" problem

---
+++ Longest common prefix/index.js
@@ -0,0 +1,11 @@
+// CHALLENGE:
+// Return a string containing the longest common prefix from a list of strings
+// EXAMPLE:
+// Input - ["flower", "florida", "flax"]
+// Output - "fl"
+
+function longestCommonPrefix(array) {
+    
+}
+
+longestCommonPrefix(["flower", "florida", "flax"]);
