f9f1db707ea43bd94a4b0b2a968281cf2d9faa4b
ravi-aratchige <rmaratchige@gmail.com>
2023-02-16 01:37:52 +0530

    Initialized repo and created TypeScript file

---
+++ Two sum/index.ts
@@ -0,0 +1,19 @@
+// Given an array of integers 'nums' and an integer 'target', return indices
+// of the two numbers such that they add up to 'target'.
+// You may assume that each input would have exactly one solution, and
+// you may not use the same element twice.
+// You can return the answer in any order.
+
+// PROBLEM SOURCE - LeetCode (https://leetcode.com/problems/two-sum/)
+
+function twoSum(nums: number[], target: number): void {
+    let num1: number, num2: number;
+    for (let i: number = 0; i < nums.length; i++) {
+        num1 = nums[i];
+        for (let j: number = 0; j < (nums.length - 1); j++) {
+            
+        }
+    }
+}
+
+twoSum([2, 7, 11, 15], 9);      // expected output = [0,1];
