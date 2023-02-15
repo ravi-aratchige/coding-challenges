ae921cec5b933da3ee26587c6ef4db7e25919e15
ravi-aratchige <rmaratchige@gmail.com>
2023-02-16 02:03:13 +0530

    Solution complete. Needs refactoring

--- Two sum/index.ts
+++ Two sum/index.ts
@@ -7,13 +7,29 @@
 // PROBLEM SOURCE - LeetCode (https://leetcode.com/problems/two-sum/)
 
 function twoSum(nums: number[], target: number): void {
-    let num1: number, num2: number;
+    let index1: number = 0, index2: number = 0;
+    let num1: number = 0, num2: number = 0;
     for (let i: number = 0; i < nums.length; i++) {
         num1 = nums[i];
-        for (let j: number = 0; j < (nums.length - 1); j++) {
-            
+        index1 = i;
+        console.log(num1);
+        for (let j: number = (i + 1); j < nums.length; j++) {
+            num2 = nums[j];
+            console.log(num2);
+            if (target === num1 + num2) {
+                index2 = j;
+                break;
+            }
+        }
+        if (target === num1 + num2) {
+            break;
         }
     }
+    console.log(`${index1}, ${index2}`);
 }
 
+// NOTE
+// code only outputs elements, not indices.
+
 twoSum([2, 7, 11, 15], 9);      // expected output = [0,1];
+twoSum([3,2,4], 6);             // expected output = [1,2];
