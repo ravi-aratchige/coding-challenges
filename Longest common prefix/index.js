7e98e621f211a22e79df5748870ba9202874cb7a
ravi-aratchige <rmaratchige@gmail.com>
2023-02-15 18:56:05 +0530

    Fixed issues of all test cases

--- Longest common prefix/index.js
+++ Longest common prefix/index.js
@@ -5,15 +5,14 @@
 // Output - "fl"
 
 function longestCommonPrefix(array) {
-    // initiate the 'prefix' variable
-    let prefix = "";
+    // initiate the 'prefix' variable using the first 2 words of the list
+    let prefix = checkCommonCharacters(array[0], array[1]);
 
-    for (let i = 0; i < array.length; i++) {
-        if (prefix === "") {
-            prefix = checkCommonCharacters(array[0], array[1]);
-            // console.log(prefix);
-            if (prefix === "") break;
-        } else prefix = checkCommonCharacters(prefix, array[i]);
+    for (let i = 2; i < array.length; i++) {
+        // if prefix of first 2 words (or any 2 other words) is empty, break and stop checking the other words
+        if (prefix === "") break;
+        // else get new prefix from current prefix and third (or next) word in array
+        else prefix = checkCommonCharacters(prefix, array[i]);
     }
     
     // return prefix after checking whether a common prefix exists
@@ -45,9 +44,4 @@ console.log(longestCommonPrefix(["flower", "florida", "flax"]));                
 console.log(longestCommonPrefix(["four", "flower", "florida", "flax"]));        // output = "f"
 console.log(longestCommonPrefix(["flower", "flower"]));                         // output = "flower"
 console.log(longestCommonPrefix(["ravindu", "flower", "florida", "flax"]));     // output = "No common prefixes"
-
-// ERROR
-console.log(longestCommonPrefix(["flower", "florida", "ravindu", "flax"]));    // expected output = "No common prefixes"
-
-// NOTE (FIXED)
-// checkCommonCharacters() gives undefined output when both arguments are the same word. Find out why this happens.
+console.log(longestCommonPrefix(["flower", "florida", "ravindu", "flax"]));     // output = "No common prefixes"
