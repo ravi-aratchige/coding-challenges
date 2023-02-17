e030156de1e74d2bef05c25db0afa872e79d41ad
ravi-aratchige <rmaratchige@gmail.com>
2023-02-17 09:00:38 +0530

    Code refactored

--- The container with the most water (solved)/index.ts
+++ The container with the most water (solved)/index.ts
@@ -3,15 +3,10 @@ function containerWithMostWater(height: number[]): number {
     for (let column1: number = 0; column1 < height.length; column1++) {
         for (let column2: number = 1; column2 < height.length; column2++) {
             // getting minimum height between columns being compared currently
-            if (height[column1] < height[column2]) {
-                minHeight = height[column1];
-            } else {
-                minHeight = height[column2];
-            }
+            if (height[column1] < height[column2]) minHeight = height[column1];
+            else minHeight = height[column2];
             // checking whether amount of water between columns being compared is greater than the greatest amount currently
-            if (amountOfWater < (minHeight * (column2 - column1))) {
-                amountOfWater = (minHeight * (column2 - column1));
-            }
+            if (amountOfWater < (minHeight * (column2 - column1))) amountOfWater = (minHeight * (column2 - column1));
         }
     }
     return amountOfWater;
