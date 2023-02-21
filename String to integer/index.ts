c3880814bdbee2c9b0b8f33f8b058613ffa891de
ravi-aratchige <rmaratchige@gmail.com>
2023-02-21 22:29:12 +0530

    Function works for inputs that do not include zero

--- String to integer/index.ts
+++ String to integer/index.ts
@@ -5,25 +5,43 @@
 function stringToInteger(str: string): number {
     let integer: number = 0;
     let intPosition: number;
-    for (let i: number = (str.length), j: number = 1; i > 0; i--, (j=j+10)) {
+    for (let i: number = (str.length - 1), j: number = 1; i >= 0; i--, (j=j*10)) {
         switch(str[i]) {
             case "1":
                 intPosition = 1;
                 break;
             case "2":
-                intPosition = 1;
+                intPosition = 2;
                 break;
             case "3":
-                intPosition = 1;
+                intPosition = 3;
                 break;
             case "4":
-                intPosition = 1;
+                intPosition = 4;
+                break;
+            case "5":
+                intPosition = 5;
+                break;
+            case "6":
+                intPosition = 6;
+                break;
+            case "7":
+                intPosition = 7;
+                break;
+            case "8":
+                intPosition = 8;
+                break;
+            case "9":
+                intPosition = 9;
                 break;
             default:
                 intPosition = 0;
-                break
+                break;
         }
-        integer = integer + (intPosition * j);
+        if (intPosition != 0) {
+            integer = integer + (intPosition * j);
+            console.log(integer);
+        }
     }
     return integer;
 }
