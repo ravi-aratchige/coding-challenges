34ddceda86a1a1b6af952cda9711a39ad5b50550
ravi-aratchige <rmaratchige@gmail.com>
2023-02-21 22:54:28 +0530

    Added code for negative numbers

--- String to integer/index.ts
+++ String to integer/index.ts
@@ -4,48 +4,57 @@
 
 function stringToInteger(str: string): number {
     let integer: number = 0;
-    let intPosition: number;
+    let digit: number;
     for (let i: number = (str.length - 1), j: number = 1; i >= 0; i--, (j=j*10)) {
         switch(str[i]) {
             case "1":
-                intPosition = 1;
+                digit = 1;
                 break;
             case "2":
-                intPosition = 2;
+                digit = 2;
                 break;
             case "3":
-                intPosition = 3;
+                digit = 3;
                 break;
             case "4":
-                intPosition = 4;
+                digit = 4;
                 break;
             case "5":
-                intPosition = 5;
+                digit = 5;
                 break;
             case "6":
-                intPosition = 6;
+                digit = 6;
                 break;
             case "7":
-                intPosition = 7;
+                digit = 7;
                 break;
             case "8":
-                intPosition = 8;
+                digit = 8;
                 break;
             case "9":
-                intPosition = 9;
+                digit = 9;
                 break;
             default:
-                intPosition = 0; 
+                digit = 0; 
                 break;
         }
-        if (intPosition != 0) {
-            integer = integer + (intPosition * j);
-            console.log(integer);
-        } // else j = j * 10;
+
+        // if digit is not a zero
+        if (digit != 0) {
+            integer = integer + (digit * j);
+            // console.log(integer);
+        }
     }
+
+    // checking for negative numbers
+    if (str[0] === "-") {
+        integer = integer * (-1);
+    }
+
     return integer;
 }
 
 console.log(stringToInteger("1234"));
 console.log(stringToInteger("9037"));
 console.log(stringToInteger("10000"));
+console.log(stringToInteger("-56"));
