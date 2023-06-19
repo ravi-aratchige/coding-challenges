0a78e4f23938d560f1bc73e9c65d800382e2f078
ravi-aratchige <rmaratchige@gmail.com>
2023-06-19 23:12:53 +0530

    Minor changes

--- Two sum (solved)/index.ts
+++ Two sum (solved)/index.ts
@@ -15,6 +15,8 @@ function twoSum(nums: number[], target: number): number[] {
     return [index1, index2];
 }
 
+// Test statements
+
 console.log(twoSum([2, 7, 11, 15], 9));      // expected output = [0,1];
 console.log(twoSum([3,2,4], 6));             // expected output = [1,2];
 console.log(twoSum([3,3], 6));               // expected output = [0,1];
